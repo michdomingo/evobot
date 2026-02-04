ba70ef4e8ea06a72b007429b14f7736c1722ad76
michdomingo <michelle.domingo@cvsu.edu.ph>
2026-02-05 00:05:08 +0800

    add express server for render

--- index.ts
+++ index.ts
@@ -1,3 +1,14 @@
+
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => res.send("Bot is alive"));
+
+app.listen(PORT, () => console.log("Web server ready"));
+
+
 import { Client, GatewayIntentBits } from "discord.js";
 import { Bot } from "./structs/Bot";
 
